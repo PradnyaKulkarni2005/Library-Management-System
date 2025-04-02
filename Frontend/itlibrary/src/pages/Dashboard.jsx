5405fb7f4b40c418613bb40c8e88704c551e1490
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2025-04-02 15:02:03 +0530

    modified dashboard to add delete functionality

--- Frontend/itlibrary/src/pages/Dashboard.jsx
+++ Frontend/itlibrary/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getBooks } from '../api';
+import { getBooks,deleteBook} from '../api';
 import './Dashboard.css'; 
 
 export default function Dashboard() {
@@ -18,7 +18,21 @@ export default function Dashboard() {
             console.error("Error fetching books:", error);
         }
     };
-
+    const handleDelete = async (bookId) => {
+        console.log("Deleting book with ID:", bookId);
+        if (window.confirm("Are you sure you want to delete this book?")) {
+            try {
+                await deleteBook(bookId);
+                
+                alert("Book deleted successfully");
+                setBooks(books.filter(book => book.Book_ID !== bookId));
+            } catch (error) {
+                console.error("Error deleting book:", error);
+            }
+        }
+        
+        
+    }
     return (
         <div className="dashboard-container">
             <h2>Books Available</h2>
@@ -30,6 +44,7 @@ export default function Dashboard() {
                         <th>Title</th>
                         <th>Category</th>
                         <th>Author</th>
+                        <th>Delete</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -39,6 +54,7 @@ export default function Dashboard() {
                             <td>{book.Title}</td>
                             <td>{book.Category}</td>
                             <td>{book.Author}</td>
+                            <td><button onClick={() => handleDelete(book.Book_ID)}>Delete</button></td>
                         </tr>
                     ))}
                 </tbody>
