1ae6749b510708c4788a4a6737c2a8081cbb4791
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2025-04-02 13:35:34 +0530

    added delete route

--- Backend/routes/bookRoutes.js
+++ Backend/routes/bookRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getBooks, addBook,updateBook } = require('../controller/bookController');
+const { getBooks, addBook,updateBook,deleteBook } = require('../controller/bookController');
 const router = express.Router();
 // Books route
 router.get('/get', getBooks);
 router.post('/add', addBook);
 router.post('/update', updateBook);
+router.delete('/delete/:bookId', deleteBook);
 
 module.exports = router;
