d633ed721103c43aa47b2e8f63b04e2929f32e73
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2025-04-04 21:55:42 +0530

    added return book route

--- Backend/routes/bookRoutes.js
+++ Backend/routes/bookRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const { getBooks, addBook,updateBook,deleteBook,issueBook } = require('../controller/bookController');
 const router = express.Router();
-// Books route
+// Get Books route
 router.get('/get', getBooks);
+// Add Book route
 router.post('/add', addBook);
+// Update Book route
 router.post('/update', updateBook);
+// Delete Book route
 router.delete('/delete/:bookId', deleteBook);
+// Issue Book route
 router.post('/issue', issueBook);
+// Return Book route
+router.post("/book/return/:issueId", returnBook);
 module.exports = router;
