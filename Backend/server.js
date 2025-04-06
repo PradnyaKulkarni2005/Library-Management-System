8ea71e6968031af1eba5f8d4af1a99adb41a5c06
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2025-04-06 21:49:06 +0530

    modified server.js to add user route

--- Backend/server.js
+++ Backend/server.js
@@ -5,12 +5,14 @@ require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
 const bookRoutes = require('./routes/bookRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/book', bookRoutes);
+app.use('/api/student',userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
